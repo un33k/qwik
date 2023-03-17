4db0c7127343c1e57390089df64afe06a3faaff7
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-03-16 21:14:00 -0400

    test for combineExec

--- packages/qwik/src/core/util/transform.ts
+++ packages/qwik/src/core/util/transform.ts
@@ -1,76 +1,24 @@
 /**
- * Decode HTML entities.
- *
- * @param html - HTML string to decode
- * @returns Decoded string
+ * Convert a function to a string, optionally executing it.
+ * @param fn - Function to convert
+ * @param execute - Whether to execute the function
+ * @returns String
+ * @public
+ * @remarks arrow functions are not supported
  */
-export const decodeHTML /*#__PURE__*/ = (html: string): string => {
-  const entities: { [id: string]: string } = {
-    amp: '&',
-    apos: "'",
-    gt: '>',
-    lt: '<',
-    nbsp: '\xa0',
-    quot: '"',
-  };
-  return html.replace(/&([a-z]+);/gi, (match: string, entity: string) => {
-    entity = entity.toLowerCase();
-    if (Object.prototype.hasOwnProperty.call(entities, entity)) {
-      return entities[entity];
-    }
-    return match;
-  });
-};
-
-/**
- * Encode HTML entities.
- *
- * @param html - HTML string to encode
- * @returns Encoded string
- */
-export const encodeHTML = /*#__PURE__*/ (html: string): string => {
-  const entities: { [id: string]: string } = {
-    '&': '&amp;',
-    "'": '&apos;',
-    '>': '&gt;',
-    '<': '&lt;',
-    '\xa0': '&nbsp;',
-    '"': '&quot;',
-  };
-  return html.replace(/[&'"<> \xa0]/g, (match: string) => {
-    return entities[match] || match;
-  });
-};
-
-// /**
-//  * Convert a function to a string, and adds execution option.
-//  *
-//  * @param fn - Function to convert to string
-//  * @param execute - Whether to execute the function
-//  * @returns String
-//  */
-// export const functionToString = (fn: Function, execute = true): string => {
-//   let toStr = fn.toString();
-//   if (execute) {
-//     const functionName = fn.name;
-//     toStr = `${toStr}\n${functionName ? functionName + '();' : ''}`;
-//   }
-//   return toStr.replace(/\s+/g, ' ').trim() + '\n';
-// };
-
 export const functionToString = (fn: Function, execute = true): string => {
-  let fnStr = fn.toString();
+  let toStr = fn.toString();
 
   // No support for arrow functions as they don't have a name.
-  if (!fnStr.startsWith('function')) {
+  if (!toStr.startsWith('function')) {
     throw new Error('Arrow functions are not supported');
   }
 
   if (execute) {
     const functionName = fn.name;
-    fnStr = `${fnStr}\n${functionName ? functionName + '();' : ''}`;
+    toStr = `${toStr}\n${functionName ? functionName + '();' : ''}`;
   }
-  return fnStr.replace(/\s+/g, ' ').trim() + '\n';
+  return toStr.replace(/\s+/g, ' ').trim() + '\n';
 };
 
 /**
@@ -81,15 +29,15 @@ export const functionToString = (fn: Function, execute = true): string => {
  * @param execute - Whether to execute the functions
  * @returns String
  */
-export function combineExecutables(fns: (string | Function)[], execute = true): string {
-  const reducer = (acc: string, fn: string | Function) => {
+export const combineExecutables = (fns: (string | Function)[], execute = true): string => {
+  const toStr = fns.reduce((acc: string, fn: string | Function) => {
     if (typeof fn === 'function') {
-      return `${acc} ${functionToString(fn, execute)} \n`;
+      return `${acc}${functionToString(fn, execute)}`;
     } else if (typeof fn === 'string') {
-      return `${acc} ${fn} \n`;
+      return `${acc}${fn}`;
     }
     throw new Error('Invalid argument type for combineExecutables');
-  };
+  }, '');
 
-  return fns.reduce(reducer, '');
-}
+  return toStr.replace(/\s+/g, ' ').trim() + '\n';
+};
--- packages/qwik/src/core/util/transform.unit.ts
+++ packages/qwik/src/core/util/transform.unit.ts
@@ -1,69 +1,53 @@
 import { suite } from 'uvu';
 import { equal, throws } from 'uvu/assert';
-import { decodeHTML, encodeHTML, functionToString } from './transform';
-
-const encodeHtmlSuite = suite('encodeHTML');
-encodeHtmlSuite('returns an empty string if the input is empty', () => {
-  equal(encodeHTML(''), '');
-});
-
-encodeHtmlSuite('encodes special characters correctly', () => {
-  const input = '<h1>Hello World!</h1>';
-  const expectedOutput = '&lt;h1&gt;Hello World!&lt;/h1&gt;';
-  equal(encodeHTML(input), expectedOutput);
-});
-
-encodeHtmlSuite('returns the input string if it contains no special characters', () => {
-  equal(encodeHTML('Hello World'), 'Hello World');
-});
-
-const decodeHtmlSuite = suite('decodeHTML');
-decodeHtmlSuite('should decode HTML entities', () => {
-  const html =
-    '&lt;div&gt;Hello &amp; World&apos;s "Greatest" Website&apos;s &nbsp;&mdash; Qwik&lt;/div&gt;';
-  const expected = '<div>Hello & World\'s "Greatest" Website\'s  — Qwik</div>';
-  equal(decodeHTML(html), expected);
-});
-
-decodeHtmlSuite('should leave non-HTML strings unchanged', () => {
-  equal(decodeHTML('Hello, world!'), 'Hello, world!');
-});
+import { combineExecutables, functionToString } from './transform';
 
 const functionToStringSuite = suite('functionToString');
 functionToStringSuite('convert a function to a string, with auto execution on', () => {
   function addFunc() {
-    const a = 1;
-    const b = 2;
+    const a = 2;
+    const b = 1;
     return a + b;
   }
   const actual = functionToString(addFunc);
   const expected =
-    `function addFunc(){const a=1;const b=2;return a+b} addFunc();`.replace(/\s+/g, ' ').trim() +
+    `function addFunc(){const a=2;const b=1;return a+b} addFunc();`.replace(/\s+/g, ' ').trim() +
     '\n';
-  equal(decodeHTML(actual), decodeHTML(expected));
+  equal(actual, expected);
 });
 
 functionToStringSuite('convert a function to a string, without executing', () => {
   function addFunc() {
-    const a = 1;
-    const b = 2;
+    const a = 2;
+    const b = 1;
     return a + b;
   }
   const actual = functionToString(addFunc, false);
   const expected =
-    `function addFunc(){const a=1;const b=2;return a+b}`.replace(/\s+/g, ' ').trim() + '\n';
-  equal(decodeHTML(actual), decodeHTML(expected));
+    `function addFunc(){const a=2;const b=1;return a+b}`.replace(/\s+/g, ' ').trim() + '\n';
+  equal(actual, expected);
 });
 
 functionToStringSuite('throw error for arrow function to string', () => {
   const addFunc = () => {
-    const a = 1;
-    const b = 2;
+    const a = 2;
+    const b = 1;
     return a + b;
   };
   throws(() => functionToString(addFunc));
 });
 
-// encodeHtmlSuite.run();
-// decodeHtmlSuite.run();
+const combineExecutablesSuite = suite('combineExecutables');
+combineExecutablesSuite('combine two functions', () => {
+  function addFunc(): number {
+    return 1 + 2;
+  }
+  function subFunc(): number {
+    return 1 - 2;
+  }
+  const actual = combineExecutables([addFunc, subFunc]);
+  const expected = `function addFunc(){return 1+2} addFunc(); function subFunc(){return 1-2} subFunc(); \n`;
+  equal(actual.replace(/\s+/g, ' '), expected.replace(/\s+/g, ' '));
+});
+combineExecutablesSuite.run();
 functionToStringSuite.run();
