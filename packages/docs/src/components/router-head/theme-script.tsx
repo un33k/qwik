f2369e9b1ba1a0e5ecb53276908eb60f3760f406
Val Neekman (AvidCoder) <un33kvu@gmail.com>
2023-03-16 21:56:52 -0400

    use tranform utils

--- packages/docs/src/components/router-head/theme-script.tsx
+++ packages/docs/src/components/router-head/theme-script.tsx
@@ -1,11 +1,19 @@
+import { combineInlines } from '@builder.io/qwik';
+
 export const themeStorageKey = 'theme-preference';
 
+function setTheme() {
+  const colorTheme = localStorage.getItem(themeStorageKey);
+  if (colorTheme === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    localStorage.setItem(themeStorageKey, colorTheme || 'dark');
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+}
+
 export const ThemeScript = () => {
-  const themeScript = `
-        document.firstElementChild
-            .setAttribute('data-theme',
-                localStorage.getItem('${themeStorageKey}') ??
-                (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-            )`;
-  return <script dangerouslySetInnerHTML={themeScript} />;
+  return <script dangerouslySetInnerHTML={combineInlines(setTheme)} />;
 };
